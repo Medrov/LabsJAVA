3ead78be9493652dc7bdf462f9f6efdf3d42db40
Mob <43564622+Medrov@users.noreply.github.com>
2023-12-08 17:57:07 +0300

    Finish Lab10.

--- src/Lab10/Lab10GUI.java
+++ src/Lab10/Lab10GUI.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -20,13 +21,15 @@ public class Lab10GUI {
 
     private JTextField brandField, modelField, yearField, priceField;
     private DefaultTableModel tableModel;
+
+    JTable table;
     public static void main(String[] args) {
         SwingUtilities.invokeLater(Lab10GUI::runApp);
     }
 
     private static void runApp() {
         try {
-            UIManager.setLookAndFeel(new FlatMacLightLaf());
+            UIManager.setLookAndFeel(new FlatMacDarkLaf());
         } catch (UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
@@ -44,14 +47,12 @@ public class Lab10GUI {
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
 
-        Font textFont = new Font("Arial", Font.PLAIN, 16);
-
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenuItem createItem = new JMenuItem("Create Record");
-        createItem.addActionListener(e -> showRecordDialog(null));
+        createItem.addActionListener(e -> showRecordDialog());
         JMenuItem deleteItem = new JMenuItem("Delete Record");
-        deleteItem.addActionListener(e -> deleteRecord());
+        deleteItem.addActionListener(e -> deleteSelectedRecord());
         JMenuItem saveItem = new JMenuItem("Save to File");
         saveItem.addActionListener(e -> saveToFile());
 
@@ -61,7 +62,7 @@ public class Lab10GUI {
 
         JMenu editMenu = new JMenu("Edit");
         JMenuItem editValueItem = new JMenuItem("Edit Value");
-        editValueItem.addActionListener(e -> editValue());
+        editValueItem.addActionListener(e -> updateRecord((String[]) getRecordData()));
         JMenuItem exitItem = new JMenuItem("Exit");
         exitItem.addActionListener(e -> System.exit(0));
 
@@ -72,12 +73,37 @@ public class Lab10GUI {
         menuBar.add(editMenu);
         frame.setJMenuBar(menuBar);
 
+        String[] columns = {"Марка", "Модель", "Год", "Цена"};
+        tableModel = new DefaultTableModel(columns, 0);
+
+        table = new JTable(tableModel) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return column != 4;
+            }
+        };
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        frame.getContentPane().add(panel);
+    }
+
+    private void showRecordDialog() {
+        JFrame recordFrame = new JFrame( "Добавить запись" );
+        recordFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        recordFrame.setLocationRelativeTo(null);
+        recordFrame.setLayout(new BorderLayout());
+        Font textFont = new Font("Arial", Font.PLAIN, 16);
+
         JPanel inputPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.WEST;
-        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.insets = new Insets(4, 16, 4, 16);
 
         JLabel brandLabel = new JLabel("Марка:");
         brandLabel.setFont(textFont);
@@ -127,114 +153,26 @@ public class Lab10GUI {
         gbc.gridwidth = 2;
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        JButton addButton = new JButton("Добавить");
-        addButton.addActionListener(e -> addRecord());
+        JButton addButton = new JButton( "Добавить");
+        addButton.addActionListener(e -> {
+            addRecord();
+            recordFrame.dispose();
+        });
         addButton.setFont(textFont);
         inputPanel.add(addButton, gbc);
 
         gbc.gridy++;
-        JButton saveButton = new JButton("Сохранить в файл");
-        saveButton.addActionListener(e -> saveToFile());
-        saveButton.setFont(textFont);
-        inputPanel.add(saveButton, gbc);
-
-        String[] columns = {"Марка", "Модель", "Год", "Цена", "Удалить"};
-        tableModel = new DefaultTableModel(columns, 0);
-
-        JTable table = new JTable(tableModel) {
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                return column != 4;
-            }
-        };
-
-        table.getColumn("Удалить").setCellRenderer(new ButtonRenderer());
-        table.getColumn("Удалить").setCellEditor(new ButtonEditor(new JCheckBox(), tableModel));
-        table.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int column = table.getColumnModel().getColumnIndexAtX(e.getX());
-                int row = e.getY() / table.getRowHeight();
-
-                if (row < table.getRowCount() && row >= 0 && column < table.getColumnCount() && column >= 0) {
-                    Object value = table.getValueAt(row, column);
-                    if (value instanceof JButton) {
-                        ((JButton) value).doClick();
-                    }
-                }
-            }
-        });
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-        panel.add(inputPanel, BorderLayout.NORTH);
-        panel.add(scrollPane, BorderLayout.CENTER);
-
-        frame.getContentPane().add(panel);
-    }
-
-    private void showRecordDialog(String[] recordData) {
-        JFrame recordFrame = new JFrame(recordData == null ? "Добавить запись" : "Изменить запись");
-        recordFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        recordFrame.setLocationRelativeTo(null);
-        recordFrame.setLayout(new BorderLayout());
-
-        JPanel inputPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.insets = new Insets(5, 5, 5, 5);
-
-        Font textFont = new Font("Arial", Font.PLAIN, 16);
-
-        JLabel brandLabel = new JLabel("Марка:");
-        brandLabel.setFont(textFont);
-        inputPanel.add(brandLabel, gbc);
-
-        gbc.gridy++;
-        JLabel modelLabel = new JLabel("Модель:");
-        modelLabel.setFont(textFont);
-        inputPanel.add(modelLabel, gbc);
-
-        //... добавляем остальные метки для других полей ...
-
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.weightx = 1.0;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-
-        brandField = new JTextField(recordData != null ? recordData[0] : "");
-        brandField.setFont(textFont);
-        inputPanel.add(brandField, gbc);
-
-        gbc.gridy++;
-        modelField = new JTextField(recordData != null ? recordData[1] : "");
-        modelField.setFont(textFont);
-        inputPanel.add(modelField, gbc);
-
-        //... добавляем остальные текстовые поля ...
-
-        gbc.gridy++;
-        gbc.gridx = 0;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-
-        JButton saveButton = new JButton(recordData == null ? "Добавить" : "Изменить");
-        saveButton.addActionListener(e -> {
-            if (recordData == null) {
-                addRecord();
-            } else {
-                updateRecord(recordData);
-            }
-            recordFrame.dispose();
-        });
-        saveButton.setFont(textFont);
-        inputPanel.add(saveButton, gbc);
-
         recordFrame.add(inputPanel, BorderLayout.NORTH);
         recordFrame.pack();
         recordFrame.setVisible(true);
+    }
+    public String[] getRecordData(){
+        String brand = brandField.getText();
+        String model = modelField.getText();
+        String year = yearField.getText();
+        String price = priceField.getText();
+
+        return new String[]{brand, model, year, price};
     }
 
     private void addRecord() {
@@ -243,69 +181,155 @@ public class Lab10GUI {
         String year = yearField.getText();
         String price = priceField.getText();
 
-        Object[] rowData = {brand, model, year, price};
-        tableModel.addRow(rowData);
+        if (tryParseInt(year) && tryParseDouble(price)) {
+            Object[] data = {brand, model, year, price};
+            tableModel.addRow(data);
 
-        // Очистка полей после добавления записи
-        brandField.setText("");
-        modelField.setText("");
-        yearField.setText("");
-        priceField.setText("");
+            brandField.setText("");
+            modelField.setText("");
+            yearField.setText("");
+            priceField.setText("");
+        } else {
+            showError("Ошибка", "Неверно написаны числовые данные");
+        }
     }
 
     private void updateRecord(String[] recordData) {
-        // Logic to update record with provided recordData
-        // Example: displayArea.replaceRange(updatedRecord, start, end);
+        JFrame editFrame = new JFrame("Редактировать запись");
+        editFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        editFrame.setLayout(new BorderLayout());
+        editFrame.setLocationRelativeTo(null);
+
+        Font textFont = new Font("Arial", Font.PLAIN, 16);
+
+        JPanel editPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(4, 16, 4, 16);
+
+        JLabel brandLabel = new JLabel("Марка:");
+        brandLabel.setFont(textFont);
+        editPanel.add(brandLabel, gbc);
+
+        gbc.gridy++;
+        JLabel modelLabel = new JLabel("Модель:");
+        modelLabel.setFont(textFont);
+        editPanel.add(modelLabel, gbc);
+
+        gbc.gridy++;
+        JLabel yearLabel = new JLabel("Год:");
+        yearLabel.setFont(textFont);
+        editPanel.add(yearLabel, gbc);
+
+        gbc.gridy++;
+        JLabel priceLabel = new JLabel("Цена:");
+        priceLabel.setFont(textFont);
+        editPanel.add(priceLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        brandField = new JTextField();
+        brandField.setFont(textFont);
+        editPanel.add(brandField, gbc);
+
+        gbc.gridy++;
+        modelField = new JTextField();
+        modelField.setFont(textFont);
+        editPanel.add(modelField, gbc);
+
+        gbc.gridy++;
+        yearField = new JTextField();
+        yearField.setFont(textFont);
+        editPanel.add(yearField, gbc);
+
+        gbc.gridy++;
+        priceField = new JTextField();
+        priceField.setFont(textFont);
+        editPanel.add(priceField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 2;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+
+        JButton saveButton = new JButton("Сохранить");
+        saveButton.addActionListener(e -> {
+            if (tryParseInt(yearField.getText()) && tryParseDouble(priceField.getText())) {
+                String[] newData = getRecordData();
+                int selectedRow = table.getSelectedRow();
+                for (int i = 0; i < newData.length; i++) {
+                    tableModel.setValueAt(newData[i], selectedRow, i);
+                }
+            } else {
+                showError("Ошибка", "Неверно написаны числовые данные");
+            }
+            editFrame.dispose();
+        });
+
+        saveButton.setFont(textFont);
+        editPanel.add(saveButton);
+
+        editFrame.add(editPanel, BorderLayout.NORTH);
+        editFrame.pack();
+        editFrame.setVisible(true);
     }
 
     private void saveToFile() {
-        //String content = displayArea.getText();
-        String content = "";
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("автомобили.txt"))) {
-            writer.write(content);
-            writer.flush();
+        File file = new File("автомобили.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (int i = 0; i < table.getRowCount(); i++) {
+                for (int j = 0; j < table.getColumnCount(); j++) {
+                    Object cellValue = table.getValueAt(i, j);
+                    writer.write(cellValue.toString());
+                    if (j < table.getColumnCount() - 1) {
+                        writer.write("\t"); // Добавляем табуляцию между ячейками
+                    }
+                }
+                writer.newLine(); // Переходим на следующую строку после завершения строки таблицы
+            }
             showAlert("Успех", "Данные сохранены в файл");
         } catch (IOException e) {
             showAlert("Ошибка", "Ошибка при сохранении данных");
+            e.printStackTrace(); // Обработайте исключение по вашему усмотрению
         }
     }
 
-    private void editValue() {
-        //String selectedRecord = displayArea.getSelectedText();
-        String selectedRecord = "displayArea.getSelectedText()";
-        if (selectedRecord != null) {
-            // Splitting the selected record into individual fields
-            String[] fields = selectedRecord.split(", ");
-            if (fields.length == 4) {
-                // Extracting values and populating text fields for editing
-                String brand = fields[0].substring(fields[0].indexOf(": ") + 2);
-                String model = fields[1].substring(fields[1].indexOf(": ") + 2);
-                String year = fields[2].substring(fields[2].indexOf(": ") + 2);
-                String price = fields[3].substring(fields[3].indexOf(": ") + 2);
-
-                brandField.setText(brand);
-                modelField.setText(model);
-                yearField.setText(year);
-                priceField.setText(price);
-            }
-        }
-    }
-
-    private void deleteRecord() {
-
-        JTable table = new JTable(tableModel);// Получаем текущую таблицу, предположим, что у вас есть метод, возвращающий текущую таблицу
-
+    public void deleteSelectedRecord() {
         int selectedRow = table.getSelectedRow();
         if (selectedRow != -1) {
-            table.remove(selectedRow);
-        } else {
-            JOptionPane.showMessageDialog(null, "Выберите строку для удаления", "Ошибка", JOptionPane.ERROR_MESSAGE);
+            tableModel.removeRow(selectedRow);
+            showAlert("Успех", "Данные удалены");
         }
-
     }
 
     private void showAlert(String title, String message) {
         JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void showError(String title, String message) {
+        JOptionPane.showMessageDialog(null, message, title, JOptionPane.ERROR_MESSAGE);
+    }
+
+    private boolean tryParseInt(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean tryParseDouble(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
